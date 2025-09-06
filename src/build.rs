a634e5cb17281f91adf6f210901bae07068c7a33
James Pierce <hello@jamespierce.dev>
2025-09-06 14:43:02 -0500

    init render template

--- src/build.rs
+++ src/build.rs
@@ -1,20 +1,50 @@
 use crate::config::load_config;
+use serde::Serialize;
+use std::fs;
 use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 
+#[derive(Serialize)]
+struct IndexData {
+    images: Vec<Image>,
+}
+
+#[derive(Serialize)]
+struct Image {
+    file_name: String,
+}
+
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let config = load_config()?;
+    let mut env = minijinja::Environment::new();
     let images_path = Path::new(&config.directories.images);
     let output_path = Path::new(&config.directories.output);
+    let mut images = Vec::new();
 
     for entry in WalkDir::new(images_path).into_iter().filter_map(|e| e.ok()) {
         println!("{:?}", entry);
         if entry.path().is_dir() {
             handle_directory(&entry)?;
         } else {
-            handle_file(&entry)?;
+            images.push(Image {
+                file_name: entry.file_name().to_string_lossy().to_string(),
+            });
+            // handle_file(&entry)?;
         }
     }
+
+    let index_html = fs::read_to_string(format!("{}/index.html", &config.directories.templates))
+        .expect("No template/index.html found in working directory.");
+    env.add_template_owned("index.html", index_html).unwrap();
+    let index_html_template = env.get_template("index.html").unwrap();
+    let index_html_rendered = index_html_template
+        .render(minijinja::context! {
+            title => &config.theme.title,
+            root_path => &config.server.root_path,
+            data => IndexData { images },
+        })
+        .unwrap();
+    fs::write(output_path.join("index.html"), index_html_rendered)?;
 
     Ok(())
 }
